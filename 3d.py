e68205ca2b771b24bf69efc52ad04702fe212663
Indcader56 <Indcader@gmail.com>
2026-07-28 19:07:01 -0700

    Completely reworked the map so that the player stays still and the map moves around them. Also added basic collision

--- 3d.py
+++ 3d.py
@@ -19,6 +19,7 @@ world_size_y = 32
 tile_color = (228,230,168)
 floor_color = (168, 142, 109)
 ceiling_color = (229, 224, 206)
+map_color = (220,220,220)
 
 ray_color = (255,255,255)
 player_color = (255,0,0)
@@ -43,8 +44,23 @@ mouse_down = False
 tile_select = 0
 hud = False
 
-#world_data = [[1 if random.randint(0,5) == 0 else 0 for x in range(world_size_x)] for y in range(world_size_y)]
-world_data = [[0 for x in range(world_size_x)] for y in range(world_size_y)]
+world_data = [[1 if random.randint(0,5) == 0 else 0 for x in range(world_size_x)] for y in range(world_size_y)]
+#world_data = [[0 for x in range(world_size_x)] for y in range(world_size_y)]
+
+# Makes the floor and ceiling textures
+celing_surf = pygame.Surface((1,3))
+floor_surf = pygame.Surface((1,3))
+
+celing_surf.set_at((0,0), ceiling_color)
+celing_surf.set_at((0,1), (ceiling_color[0]/2, ceiling_color[1]/2, ceiling_color[2]/2))
+celing_surf.set_at((0,2), (0,0,0))
+
+floor_surf.set_at((0,0), (0,0,0))
+floor_surf.set_at((0,1), (floor_color[0]/2, floor_color[1]/2, floor_color[2]/2))
+floor_surf.set_at((0,2), floor_color)
+
+gradient_celing_surf = pygame.transform.smoothscale(celing_surf, (window_size[0], window_size[1]/2))
+gradient_floor_surf = pygame.transform.smoothscale(floor_surf, (window_size[0], window_size[1]/2))
 
 while True:
     mouse = pygame.mouse.get_pos()
@@ -87,25 +103,35 @@ while True:
             if event.type == pygame.MOUSEBUTTONUP:
                 mouse_down = False
                 
-
     player_dir = dir*(math.pi/180)
 
     if click_w == True:
-        player_x += math.cos(player_dir)*player_speed
-        player_y += math.sin(player_dir)*player_speed
+        new_x = player_x + math.cos(player_dir)*player_speed
+        new_y = player_y + math.sin(player_dir)*player_speed
+        if world_data[round(new_y)//tile_size_y][round(new_x)//tile_size_x] == 0:
+            player_x = new_x
+            player_y = new_y
+
     if click_s == True:
-        player_x -= math.cos(player_dir)*player_speed
-        player_y -= math.sin(player_dir)*player_speed
+        new_x = player_x - math.cos(player_dir)*player_speed
+        new_y = player_y - math.sin(player_dir)*player_speed
+        if world_data[round(new_y)//tile_size_y][round(new_x)//tile_size_x] == 0:
+            player_x = new_x
+            player_y = new_y
+
     if click_left == True:
         dir -= 2
     if click_right == True:
         dir += 2
 
     if mouse_down == True:
-        try:
-            world_data[mouse_y//tile_size_y][mouse_x//tile_size_x] = tile_select
-        except:
-            pass
+        mouse_block_x = round((mouse_x-window_size[0]/2)+player_x)//tile_size_x
+        mouse_block_y = round((mouse_y-window_size[1]/2)+player_y)//tile_size_y
+
+        if mouse_block_y < world_size_y and mouse_block_y >= 0:
+            if mouse_block_x < world_size_x and mouse_block_x >= 0:
+                world_data[round((mouse_y-window_size[1]/2)+player_y)//tile_size_y][round((mouse_x-window_size[0]/2)+player_x)//tile_size_x] = tile_select
+ 
 
     if player_dir > 360:
         dir = 0
@@ -119,42 +145,28 @@ while True:
 
     # Draws the floor and ceiling gradients
     screen.fill((0,0,0))
-    celing_surf = pygame.Surface((1,3))
-    floor_surf = pygame.Surface((1,3))
-
-    celing_surf.set_at((0,0), ceiling_color)
-    celing_surf.set_at((0,1), (ceiling_color[0]/2, ceiling_color[1]/2, ceiling_color[2]/2))
-    celing_surf.set_at((0,2), (0,0,0))
-
-    floor_surf.set_at((0,0), (0,0,0))
-    floor_surf.set_at((0,1), (floor_color[0]/2, floor_color[1]/2, floor_color[2]/2))
-    floor_surf.set_at((0,2), floor_color)
-
-    gradient_celing_surf = pygame.transform.smoothscale(celing_surf, (window_size[0], window_size[1]/2))
-    gradient_floor_surf = pygame.transform.smoothscale(floor_surf, (window_size[0], window_size[1]/2))
 
     screen.blit(gradient_celing_surf, (0,0))
     screen.blit(gradient_floor_surf, (0,window_size[1]/2))
-
 
     # Raycaster
     lines = []
     ray_lines = []
     for x in range(240):
-        ray_x = player_x
-        ray_y = player_y
+        ray_x = 0
+        ray_y = 0
         d = (dir+((x*0.375)-45))*(math.pi/180)
 
         while True:
             ray_x += (math.cos(d)*1)
             ray_y += (math.sin(d)*1)
 
-            world_ray_x = round(ray_x//tile_size_x)
-            world_ray_y = round(ray_y//tile_size_y)
+            world_ray_x = round((ray_x+player_x)//tile_size_x)
+            world_ray_y = round((ray_y+player_y)//tile_size_y)
 
             if world_ray_x >= 0 and world_ray_x < world_size_x and world_ray_y >= 0 and world_ray_y < world_size_y:
                 if world_data[world_ray_y][world_ray_x] == 1:
-                    lines.append(math.sqrt(((ray_x-player_x)**2)+((ray_y-player_y)**2))* math.cos(d-player_dir))
+                    lines.append(math.sqrt(((ray_x)**2)+((ray_y)**2))* math.cos(d-player_dir))
                     break
 
             else:
@@ -178,18 +190,20 @@ while True:
 
             pygame.draw.line(screen, line_color, (i*4, (-line_height/2) + 360), (i*4, (line_height/2) + 360), 4)
 
-    # Draws the map if the Hud is enabled
+    # Draws the map if the hud is enabled
     if hud == True:
         for y in range(world_size_y):
             for x in range(world_size_x):
                 if world_data[y][x] == 1:
-                    pygame.draw.rect(screen, tile_color, (tile_size_x*x,tile_size_y*y,tile_size_x,tile_size_y))
+                    pygame.draw.rect(screen, map_color, (((tile_size_x*x)-player_x)+window_size[0]/2,((tile_size_y*y)-player_y)+window_size[1]/2,tile_size_x,tile_size_y))
 
         for ray in ray_lines:
-            pygame.draw.line(screen, ray_color, (player_x,player_y), (ray[0], ray[1]), 1)
+            pygame.draw.line(screen, ray_color, (window_size[0]/2,window_size[1]/2), (ray[0]+window_size[0]/2, ray[1]+window_size[1]/2), 1)
 
-        pygame.draw.circle(screen, player_color, (player_x, player_y), player_size)
-        pygame.draw.line(screen, player_color, (player_x, player_y), (player_x + (math.cos(player_dir)*20),player_y+(math.sin(player_dir)*20)))
+        pygame.draw.circle(screen, player_color, (window_size[0]/2,window_size[1]/2), player_size)
+        pygame.draw.line(screen, player_color, (window_size[0]/2,window_size[1]/2), ((math.cos(player_dir)*20)+window_size[0]/2,(math.sin(player_dir)*20)+window_size[1]/2))
+
+        pygame.draw.rect(screen, map_color, ((-player_x)+window_size[0]/2,(-player_y)+window_size[1]/2,tile_size_x*world_size_x,tile_size_y*world_size_y), 4)
 
     pygame.display.flip()
 
